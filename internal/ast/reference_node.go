992362142714d38dc88af19eed659834009f2e1d
matkrin <matkrin@protonmail.com>
2026-02-08 08:01:23 +0100

    refactor(ast): No additional traversing for RefNodes

--- internal/ast/reference_node.go
+++ internal/ast/reference_node.go
@@ -1,7 +1,6 @@
 package ast
 
 import (
-	"fmt"
 	"log/slog"
 
 	"github.com/matkrin/bashd/internal/lsp"
@@ -42,154 +41,132 @@ func (r *RefNode) ToLspTextEdit(newText string) lsp.TextEdit {
 	}
 }
 
-// Identify DefNodes with tracking processed scoped assignments to avoid duplicates
-func (a *Ast) RefNodes(includeDeclaration bool) []RefNode {
-	refNodes := []RefNode{}
+func paramExpToRefNode(paramExp *syntax.ParamExp) *RefNode {
+	if paramExp.Param == nil {
+		return nil
+	}
 
-	processedScopedAssignments := make(map[string]bool) // key: "line:col:name"
+	name := paramExp.Param.Value
+	startLine, startChar := paramExp.Param.Pos().Line(), paramExp.Param.Pos().Col()
+	endLine, endChar := paramExp.Param.End().Line(), paramExp.Param.End().Col()
 
-	syntax.Walk(a.File, func(node syntax.Node) bool {
-		var name string
-		var startLine, startChar, endLine, endChar uint
+	return &RefNode{
+		Node:      paramExp,
+		Name:      name,
+		Scope:     nil,
+		StartLine: startLine,
+		StartChar: startChar,
+		EndLine:   endLine,
+		EndChar:   endChar,
+	}
+}
 
-		switch n := node.(type) {
-		// Variable usage
-		case *syntax.ParamExp:
-			if n.Param != nil {
-				name = n.Param.Value
-				startLine, startChar = n.Param.Pos().Line(), n.Param.Pos().Col()
-				endLine, endChar = n.Param.End().Line(), n.Param.End().Col()
-			}
+func callExprToRefNode(callExpr *syntax.CallExpr, scope *syntax.FuncDecl, includeDeclaration bool) *RefNode {
+	var name string
+	var startLine, startChar, endLine, endChar uint
 
-		// Function usage (commands) and read statements
-		case *syntax.CallExpr:
-			if len(n.Args) > 0 {
-				cmdName := ExtractIdentifier(n.Args[0])
+	if len(callExpr.Args) > 0 {
+		cmdName := ExtractIdentifier(callExpr.Args[0])
 
-				// Variable assignments as part of read statements
-				if cmdName == "read" && includeDeclaration {
-					for _, arg := range n.Args[1:] {
-						for _, wp := range arg.Parts {
-							switch p := wp.(type) {
-							case *syntax.Lit:
-								name = p.Value
-								startLine, startChar = p.Pos().Line(), p.Pos().Col()
-								endLine, endChar = p.End().Line(), p.End().Col()
-							}
-						}
-					}
-				} else if cmdName != "" && cmdName != "local" && cmdName != "declare" && cmdName != "typeset" && cmdName != "read" {
-					// Function calls (except scoping commands or read statements)
-					arg := n.Args[0]
-					for _, wp := range arg.Parts {
-						switch p := wp.(type) {
-						case *syntax.Lit:
-							name = p.Value
-							startLine, startChar = p.Pos().Line(), p.Pos().Col()
-							endLine, endChar = p.End().Line(), p.End().Col()
-						}
+		// Variable assignments as part of read statements
+		if cmdName == "read" && includeDeclaration {
+			for _, arg := range callExpr.Args[1:] {
+				for _, wp := range arg.Parts {
+					switch p := wp.(type) {
+					case *syntax.Lit:
+						name = p.Value
+						startLine, startChar = p.Pos().Line(), p.Pos().Col()
+						endLine, endChar = p.End().Line(), p.End().Col()
 					}
 				}
 			}
-
-		// Function declaration
-		case *syntax.FuncDecl:
-			if n.Name != nil && includeDeclaration {
-				name = n.Name.Value
-				startLine, startChar = n.Name.Pos().Line(), n.Name.Pos().Col()
-				endLine, endChar = n.Name.End().Line(), n.Name.End().Col()
-			}
-
-		// Variable assignment without `local`, `declare`, `typeset`
-		case *syntax.Assign:
-			if n.Name != nil && includeDeclaration {
-				name = n.Name.Value
-				startLine, startChar = n.Name.Pos().Line(), n.Name.Pos().Col()
-				endLine, endChar = n.Name.End().Line(), n.Name.End().Col()
-
-				assignmentKey := fmt.Sprintf("%d:%d:%s", startLine, startChar, name)
-				if processedScopedAssignments[assignmentKey] {
-					return true
-				}
-			}
-
-		// Scoped variable declarations with `local`, `declare`, `typeset`
-		case *syntax.DeclClause:
-			if includeDeclaration {
-				cmd := n.Variant.Value
-				if cmd == "local" || cmd == "declare" || cmd == "typeset" {
-					for _, arg := range n.Args {
-						if arg.Name != nil {
-							name = arg.Name.Value
-							startLine, startChar = arg.Name.ValuePos.Line(), arg.Name.ValuePos.Col()
-							endLine, endChar = arg.Name.ValueEnd.Line(), arg.Name.ValueEnd.Col()
-
-							// Mark this assignment as processed to avoid duplicate from *syntax.Assign
-							assignmentKey := fmt.Sprintf("%d:%d:%s", startLine, startChar, name)
-							processedScopedAssignments[assignmentKey] = true
-
-							// Create a separate RefNode for each variable in the declaration
-							cursor := Cursor{Line: startLine, Col: startChar}
-							scope := a.findEnclosingFunction(cursor)
-
-							refNodes = append(refNodes, RefNode{
-								Node:      n,
-								Name:      name,
-								Scope:     scope,
-								StartLine: startLine,
-								StartChar: startChar,
-								EndLine:   endLine,
-								EndChar:   endChar,
-							})
-						}
-					}
-					return true
-				}
-			}
-
-		// Iteration variable in for/select loops and C-style loops
-		case *syntax.ForClause:
-			if includeDeclaration {
-				switch loop := n.Loop.(type) {
-				// for/select
-				case *syntax.WordIter:
-					if loop.Name != nil {
-						name = loop.Name.Value
-						startLine, startChar = loop.Name.Pos().Line(), loop.Name.Pos().Col()
-						endLine, endChar = loop.Name.End().Line(), loop.Name.End().Col()
-					}
-				// C-style
-				case *syntax.CStyleLoop:
-					if loop.Init != nil {
-						a, ok := loop.Init.(*syntax.BinaryArithm)
-						if !ok {
-							return true
-						}
-						if a.Op == syntax.Assgn {
-							word, ok := a.X.(*syntax.Word)
-							if !ok {
-								return true
-							}
-							for _, wp := range word.Parts {
-								switch p := wp.(type) {
-								case *syntax.Lit:
-									name = p.Value
-									startLine, startChar = p.Pos().Line(), p.Pos().Col()
-									endLine, endChar = p.End().Line(), p.End().Col()
-								}
-							}
-						}
-					}
+		} else if cmdName != "" && cmdName != "local" && cmdName != "declare" && cmdName != "typeset" && cmdName != "read" {
+			arg := callExpr.Args[0]
+			for _, wp := range arg.Parts {
+				switch p := wp.(type) {
+				case *syntax.Lit:
+					name = p.Value
+					startLine, startChar = p.Pos().Line(), p.Pos().Col()
+					endLine, endChar = p.End().Line(), p.End().Col()
 				}
 			}
 		}
+	}
 
-		if name != "" {
-			cursor := Cursor{Line: startLine, Col: startChar}
-			scope := a.findEnclosingFunction(cursor)
+	if name == "" {
+		return nil
+	}
+
+	return &RefNode{
+		Node:      callExpr,
+		Name:      name,
+		Scope:     scope,
+		StartLine: startLine,
+		StartChar: startChar,
+		EndLine:   endLine,
+		EndChar:   endChar,
+	}
+}
+
+func funcDeclToRefNode(funcDecl *syntax.FuncDecl, includeDeclaration bool) *RefNode {
+	if funcDecl.Name == nil || !includeDeclaration {
+		return nil
+	}
+
+	name := funcDecl.Name.Value
+	startLine, startChar := funcDecl.Name.Pos().Line(), funcDecl.Name.Pos().Col()
+	endLine, endChar := funcDecl.Name.End().Line(), funcDecl.Name.End().Col()
+
+	return &RefNode{
+		Node:      funcDecl,
+		Name:      name,
+		Scope:     nil,
+		StartLine: startLine,
+		StartChar: startChar,
+		EndLine:   endLine,
+		EndChar:   endChar,
+	}
+}
+
+func assignToRefNode(assignNode *syntax.Assign, includeDeclaration bool) *RefNode {
+	if assignNode.Name == nil || !includeDeclaration {
+		return nil
+	}
+
+	name := assignNode.Name.Value
+	startLine, startChar := assignNode.Name.Pos().Line(), assignNode.Name.Pos().Col()
+	endLine, endChar := assignNode.Name.End().Line(), assignNode.Name.End().Col()
+
+	return &RefNode{
+		Node:      assignNode,
+		Name:      name,
+		Scope:     nil,
+		StartLine: startLine,
+		StartChar: startChar,
+		EndLine:   endLine,
+		EndChar:   endChar,
+	}
+}
+
+func declClauseToRefNode(declClause *syntax.DeclClause, scope *syntax.FuncDecl, includeDeclaration bool) []RefNode {
+	if !includeDeclaration {
+		return nil
+	}
+
+	cmd := declClause.Variant.Value
+	if cmd != "local" && cmd != "declare" && cmd != "typeset" {
+		return nil
+	}
+
+	var refNodes []RefNode
+	for _, arg := range declClause.Args {
+		if arg.Name != nil {
+			name := arg.Name.Value
+			startLine, startChar := arg.Name.ValuePos.Line(), arg.Name.ValuePos.Col()
+			endLine, endChar := arg.Name.ValueEnd.Line(), arg.Name.ValueEnd.Col()
 
 			refNodes = append(refNodes, RefNode{
-				Node:      node,
+				Node:      declClause,
 				Name:      name,
 				Scope:     scope,
 				StartLine: startLine,
@@ -198,14 +175,124 @@ func (a *Ast) RefNodes(includeDeclaration bool) []RefNode {
 				EndChar:   endChar,
 			})
 		}
+	}
 
-		return true
+	return refNodes
+}
+
+func forClauseToRefNode(forClause *syntax.ForClause, includeDeclaration bool) *RefNode {
+	if !includeDeclaration {
+		return nil
+	}
+
+	var name string
+	var startLine, startChar, endLine, endChar uint
+
+	switch loop := forClause.Loop.(type) {
+	case *syntax.WordIter:
+		if loop.Name != nil {
+			name = loop.Name.Value
+			startLine, startChar = loop.Name.Pos().Line(), loop.Name.Pos().Col()
+			endLine, endChar = loop.Name.End().Line(), loop.Name.End().Col()
+		}
+
+	case *syntax.CStyleLoop:
+		if loop.Init != nil {
+			a, ok := loop.Init.(*syntax.BinaryArithm)
+			if !ok {
+				return nil
+			}
+			if a.Op == syntax.Assgn {
+				word, ok := a.X.(*syntax.Word)
+				if !ok {
+					return nil
+				}
+				for _, wp := range word.Parts {
+					switch p := wp.(type) {
+					case *syntax.Lit:
+						name = p.Value
+						startLine, startChar = p.Pos().Line(), p.Pos().Col()
+						endLine, endChar = p.End().Line(), p.End().Col()
+					}
+				}
+			}
+		}
+	}
+
+	return &RefNode{
+		Node:      forClause,
+		Name:      name,
+		Scope:     nil,
+		StartLine: startLine,
+		StartChar: startChar,
+		EndLine:   endLine,
+		EndChar:   endChar,
+	}
+
+}
+
+func syntaxNodeToRefNode(node syntax.Node, scope *syntax.FuncDecl, includeDeclaration bool) ([]RefNode, bool) {
+	descent := true
+	var refNodes []RefNode
+	switch n := node.(type) {
+	case *syntax.Assign:
+		if refNode := assignToRefNode(n, includeDeclaration); refNode != nil {
+			refNodes = append(refNodes, *refNode)
+		}
+
+	case *syntax.DeclClause:
+		if nodes := declClauseToRefNode(n, scope, includeDeclaration); len(nodes) > 0 {
+			refNodes = append(refNodes, nodes...)
+		}
+		descent = false
+
+	case *syntax.ForClause:
+		if refNode := forClauseToRefNode(n, includeDeclaration); refNode != nil {
+			refNodes = append(refNodes, *refNode)
+		}
+
+	case *syntax.CallExpr:
+		if refNode := callExprToRefNode(n, scope, includeDeclaration); refNode != nil {
+			refNodes = append(refNodes, *refNode)
+		}
+
+	case *syntax.FuncDecl:
+		if refNode := funcDeclToRefNode(n, includeDeclaration); refNode != nil {
+			refNodes = append(refNodes, *refNode)
+		}
+		syntax.Walk(n.Body, func(innerNode syntax.Node) bool {
+			nodes, descent := syntaxNodeToRefNode(innerNode, n, includeDeclaration)
+			if len(nodes) > 0 {
+				refNodes = append(refNodes, nodes...)
+			}
+			return descent
+		})
+		descent = false
+
+	case *syntax.ParamExp:
+		if refNode := paramExpToRefNode(n); refNode != nil {
+			refNodes = append(refNodes, *refNode)
+		}
+	}
+
+	return refNodes, descent
+}
+
+func (a *Ast) RefNodes(includeDeclaration bool) []RefNode {
+	refNodes := []RefNode{}
+
+	syntax.Walk(a.File, func(node syntax.Node) bool {
+		nodes, descent := syntaxNodeToRefNode(node, nil, includeDeclaration)
+		if len(nodes) > 0 {
+			refNodes = append(refNodes, nodes...)
+		}
+
+		return descent
 	})
 
 	return refNodes
 }
 
-// Find RefNodes in a file
 func (a *Ast) FindRefsInFile(cursor Cursor, includeDeclaration bool) []RefNode {
 	cursorNode := a.FindNodeUnderCursor(cursor)
 	targetIdentifier := ExtractIdentifier(cursorNode)
@@ -216,7 +303,6 @@ func (a *Ast) FindRefsInFile(cursor Cursor, includeDeclaration bool) []RefNode {
 
 	references := []RefNode{}
 
-	// Find the definition that the cursor is pointing to
 	defNode := a.FindDefInFile(cursor)
 
 	slog.Info("FINDREFS", "DEFNODE", defNode)
@@ -259,7 +345,7 @@ func (a *Ast) wouldResolveToSameDefinition(refCursorNode syntax.Node, targetDefN
 		for _, defNode := range a.DefNodes() {
 			if defNode.Name == targetIdentifier && defNode.IsScoped && defNode.Scope == refScope {
 				// Check if the scoped variable is declared BEFORE the reference position
-				if defNode.isBeforeCursor( cursor) {
+				if defNode.isBeforeCursor(cursor) {
 					// Among all local variables declared before this reference,
 					// find the one that's closest (latest declaration)
 					if closestLocalDef == nil || defNode.isDefinitionAfter(closestLocalDef) {
